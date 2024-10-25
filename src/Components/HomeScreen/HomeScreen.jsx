c92ba377f188a1ac9aeff968f8c5d2e3bc111794
mohamed.bendary <mohamedyossif577@gmail.com>
2024-10-26 02:36:59 +0400

    implement primaryNavbar component

--- src/Components/HomeScreen/HomeScreen.jsx
+++ src/Components/HomeScreen/HomeScreen.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SubNavbar from '../General/SubNavbarComponent/SubNavbar';
+import PrimaryNavbar from '../General/PrimaryNavbarComponent/PrimaryNavbar';
 
 const HomeScreen = () => {
   return (
     <div>
       <SubNavbar />
+      <PrimaryNavbar />
     </div>
   );
 };
