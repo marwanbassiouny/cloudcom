ea4e8b997a44f7dcf361ba024a3e6b6b8488313c
mohamed.bendary <mohamedyossif577@gmail.com>
2024-10-26 03:44:58 +0400

    add datamanagement component

---
+++ src/Components/HomeScreen/DataManagementComponent/DataManagement.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './DataManagement.css';
+import dataManagementImage from '../../../Assets/Icons/HomeScreen/DataManagementComponent/DataManagementImage.svg'; 
+import blueElipseIcon from '../../../Assets/Icons/HomeScreen/DataManagementComponent/bluElipse.svg'; 
+import whiteElipseIcon from '../../../Assets/Icons/HomeScreen/DataManagementComponent/whiteElipse.svg'; 
+import textContent from '../../../Assets/Data/HomeScreen/DataManagementComponent/DataManagement.json'
+
+const DataManagement = () => {
+  return (
+    <div className="data-management-container">
+      {/* Top Section: Grey Text */}
+      <div className="top-section">
+        <h1>{textContent.dataManagementService1} · {textContent.dataManagementService2} · {textContent.dataManagementService3} . {textContent.dataManagementService4} . {textContent.dataManagementService5}</h1>
+      </div>
+
+      {/* Bottom Three Parallel Sections */}
+      <div className="bottom-section">
+        {/* Left Section: Title and Description */}
+        <div className="left-section">
+          <h2>{textContent.headline1}</h2>
+          <p>{textContent.description}</p>
+          <a href="#" className="explore-btn">{textContent.exploreButton} ↗</a>
+        </div>
+
+        {/* Middle Section: Image */}
+        <div className="middle-section">
+          <img src={dataManagementImage} alt="Customer Data Management" />
+        </div>
+
+        {/* Right Section: Ellipses */}
+        <div className="right-section">
+          <img src={blueElipseIcon} className="ellipse"/>
+          <img src={whiteElipseIcon} className="ellipse"/>
+          <img src={whiteElipseIcon} className="ellipse"/>
+          <img src={whiteElipseIcon} className="ellipse"/>
+          <img src={whiteElipseIcon} className="ellipse"/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DataManagement;
--- src/Components/HomeScreen/HomeScreen.jsx
+++ src/Components/HomeScreen/HomeScreen.jsx
@@ -3,6 +3,7 @@ import SubNavbar from '../General/SubNavbarComponent/SubNavbar';
 import PrimaryNavbar from '../General/PrimaryNavbarComponent/PrimaryNavbar';
 import Intro from './IntroComponent/Intro';
 import Clients from './ClientsComponent/Clients';
+import DataManagement from './DataManagementComponent/DataManagement';
 
 const HomeScreen = () => {
   return (
@@ -11,6 +12,7 @@ const HomeScreen = () => {
       <PrimaryNavbar />
       <Intro />
       <Clients />
+      <DataManagement />
     </div>
   );
 };
