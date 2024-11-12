6ad11b9239f08eb8dae9469f72382771d1844545
mohamed.bendary <mohamedyossif577@gmail.com>
2024-11-12 06:30:49 +0400

    adjust responsive design

--- src/Components/HomeScreen/TestimonialsComponent/Testimonials.jsx
+++ src/Components/HomeScreen/TestimonialsComponent/Testimonials.jsx
@@ -13,7 +13,7 @@ const testimonials = [
     image: img, // Replace with actual image URL
     name: "Mustafa Gamal, Marketing Director at Fatura",
     job: "CPaaS – Customer Data Management – WhatsApp Channel",
-    rating: 4.5,
+    rating: 5,
     testimonial:
       "cloudcom’s CPaaS platform has transformed the way we manage our customer data at Fatura. The seamless integration with the WhatsApp channel has allowed us to engage our customers in real time, making our communication more personalized and efficient. Their support team is outstanding, and we’ve seen significant improvements in our customer relationships thanks to cloudcom’s solutions.",
   },
@@ -21,7 +21,7 @@ const testimonials = [
     image: img,
     name: "Mohamed Seif, Commercial Director at Redbull Mobile KSA",
     job: "CPaaS – Customer Value Management – SMS Channel",
-    rating: 4.5,
+    rating: 5,
     testimonial:
       "Working with CloudCom has been a game-changer for us at Redbull Mobile KSA. Their SMS channel has helped us take our customer value management to the next level. The ability to reach our customers directly and measure engagement has significantly boosted our retention rates. We’re extremely satisfied with the results and the ongoing support from the CloudCom team",
   },
@@ -29,7 +29,7 @@ const testimonials = [
     image: img,
     name: "Mahmoud El Far, CEO at Elfar Market",
     job: "SMS Channel & OTP Verify",
-    rating: 4.5,
+    rating: 5,
     testimonial:
       "CloudCom’s SMS and OTP verification solutions have been critical for us at Elfar Market. They’ve enabled us to ensure secure and reliable customer interactions, whether for account verification or promotional messaging. The simplicity and effectiveness of their services have made our processes more efficient, and we couldn’t be happier with the results.",
   },
